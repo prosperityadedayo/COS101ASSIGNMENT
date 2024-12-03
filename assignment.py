8fbe97988a3264d19af9649406b57c4e0df7a29e
prosperityadedayo <prosperity.adedayo@binghamuni.edu.ng>
2024-12-03 22:29:34 +0100

    first commit

---
+++ assignment.py
@@ -0,0 +1,11 @@
+number_list = [3,5,68,2,19,30,5,1,19,4]
+
+index = 0
+while number_list[index:]:
+    index += 1
+
+    number_index = index - 1
+
+for number in range(number_index, -1, -1):
+
+    print(number_list[number])
